fdf1444afff324cada267165634194661a28dffd
Tsadkan Kelemework <tsadkankelemeworktw@gmail.com>
2025-10-28 18:14:55 +0300

    changes on about

--- src/Components/Home/Hero.tsx
+++ src/Components/Home/Hero.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import heroimage from '../../assets/image1.png'
+import heroimage from "../../assets/image1.png"
 export default function Hero() {
   // const heroImagePath = "/images/hero-image.jpg" // Update this path with your image file
   const [isHovered, setIsHovered] = useState(false)
@@ -20,18 +20,26 @@ export default function Hero() {
         />
       </div>
 
-      {/* Content Overlay */}
-      <div className="relative z-10 text-center px-6">
+      <div className="absolute top-8 right-8 z-20">
         <a
           href="#contact"
           className="inline-block text-gray-900 font-medium text-lg border-b-2 border-purple-600 pb-2 hover:text-purple-600 transition"
         >
           Get in touch
         </a>
-        <h1 className="text-6xl md:text-6xl font-medium text-gray-700 mb-4">“Bringing creativity, code, and Odoo together to build what matters.”</h1>
-        <p className="text-xl md:text-5xl text-gray-700 font-medium mb-12">UI/UX Designer | Full Stack Developer and Odoo Implementer</p>
+      </div>
 
-        
+      {/* Content Overlay */}
+      <div className="relative z-10 text-center px-6">
+        <h1
+          className="text-6xl md:text-4xl font-medium text-gray-900 mb-4"
+          style={{ fontFamily: "'Playfair Display', serif" }}
+        >
+          "Bringing creativity, code, and Odoo together to build what matters."
+        </h1>
+        <p className="text-xl md:text-3xl text-gray-700 font-medium mb-12">
+          UI/UX Designer | Full Stack Developer and Odoo Implementer
+        </p>
       </div>
     </section>
   )
